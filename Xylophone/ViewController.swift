32590231137f0c1980960a1f6327a4978f32167a
Kevin Koshy <kevin999koshy@gmail.com>
2022-02-24 14:47:29 +0530

    Adding Playback

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -2,20 +2,31 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Kevin Koshy on 02/24/2022.
+//  Copyright © 2022 Kevin Koshy. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+        
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
 }
 
